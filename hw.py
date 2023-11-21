8e6b02966ba6d3fe904d7fc7de74439a9dd33e57
MurMurichOG <muratmuhutdinov1122@gmail.com>
2023-11-21 16:30:50 +0600

    done

--- hw.py
+++ hw.py
@@ -1,85 +1,57 @@
-class SuperHero:
-    people = 'people'
-    fly = False
-    def __init__(self, name, nickname, superpower, health_points, catchphrase):
-        self.name = name
-        self.nickname = nickname
-        self.superpower = superpower
-        self.health_points = health_points
-        self.catchphrase = catchphrase
-    def double(self):
-        self.health_points *= 2
-    def tprint(self):
-        print(self.name)
-    def __str__(self):
-        return f'{self.nickname}, {self.superpower}, {self.health_points}'
-    def __len__(self):
-        return len(self.catchphrase)
-class earth(SuperHero):
-    people = 'people'
-    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
-        self.name = name
-        self.nickname = nickname
-        self.superpower = superpower
-        self.health_points = health_points
-        self.catchphrase = catchphrase
-        self.damage = damage
-    def square(self):
-        self.health_points = self.health_points ** 2
-        self.fly = True
-    def tprint(self):
-        print(self.name)
-    def __str__(self):
-        return f'{self.nickname}, {self.superpower}, {self.health_points}'
-    def __len__(self):
-        return len(self.catchphrase)
-class air(SuperHero):
-    people = 'people'
-    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
-        self.name = name
-        self.nickname = nickname
-        self.superpower = superpower
-        self.health_points = health_points
-        self.catchphrase = catchphrase
-        self.damage = damage
-    def square(self):
-        self.health_points = self.health_points ** 2
-        self.fly = True
-    def tprint(self):
-        print(self.name)
-    def __str__(self):
-        return f'{self.nickname}, {self.superpower}, {self.health_points}'
-    def __len__(self):
-        return len(self.catchphrase)
-class villain(air):
-    people = 'monster'
-    def gen_x(self):
-        pass
-    def crit(self):
-        self.damage = self.damage ** 2
-    def tprint(self):
-        print(self.name)
-    def __str__(self):
-        return f'{self.nickname}, {self.superpower}, {self.health_points}'
-    def __len__(self):
-        return len(self.catchphrase)
-Hero = SuperHero('Mark', 'Mirage', 'Fire bending', 100, "Where there's smoke, there's fire.")
-Hero.tprint()
-Hero.double()
-print(str(Hero))
-print(len(Hero))
-Hero2 = air('Angela', 'Angel', 'Air bending', 100, 'Time to fly!', 10)
-Hero2.tprint()
-Hero2.square()
-print(str(Hero2))
-print(len(Hero2))
-Hero3 = earth('Ben', 'The Rock', 'Earth bending', 100, 'Eruption!', 2)
-Hero3.tprint()
-Hero3.square()
-print(str(Hero3))
-print(len(Hero3))
-Villain = villain('Dan', 'Ghost', 'Shapeshifting', 100, 'Good night.', 1)
-Villain.tprint()
-Villain.square()
-print(str(Villain))
-print(len(Villain))
+class Bank:
+    def __init__(self, name, money, balance2):
+        self._name = name
+        self._balance = money
+        self.balance2 = balance2
+    def moneyX(self):
+        balance = int(input('enter your addon: '))
+        self._balance += balance
+    def _kill(self):
+        self._balance = 0
+    def __jackpot(self):
+        self._balance *= 10
+    def _jackpot(self):
+        self.__jackpot()
+    def _add(self):
+        self._balance += self.balance2
+    def get_balance(self):
+        return self._balance
+a = Bank('Murat', 0, 120)
+a.moneyX()
+a._kill()
+a._jackpot()
+a._add()
+print(a._name)
+print(a.get_balance())
+class calc:
+    def __init__(self, a, b, sign):
+        self.a = a
+        self.b = b
+        self.sign = sign
+    def a1(self):
+        self.a = int(input('Enter first number: '))
+    def b1(self):
+        self.b = int(input('Enter second number: '))
+    def sign1(self):
+        self.sign = input('Enter sign: ')
+    def __add__(self, other):
+        return calc(self.a + other.b, self.b, self.sign)
+    def __sub__(self, other):
+        return calc(self.a - other.b, self.b, self.sign)
+    def __mul__(self, other):
+        return calc(self.a * other.b, self.b, self.sign)
+    def __truediv__(self, other):
+        return calc(self.a / other.b, self.b, self.sign)
+equation = calc(0, 0, 'O')
+equation.a1()
+equation.b1()
+equation.sign1()
+if equation.sign == '+':
+    result = equation + equation
+elif equation.sign == '-':
+    result = equation - equation
+elif equation.sign == '*':
+    result = equation * equation
+elif equation.sign == '/':
+    result = equation / equation
+print(result.a)
